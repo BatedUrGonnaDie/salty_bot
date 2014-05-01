b39f6bccb64583b703b35c62057b673e3bcd14ec
BatedUrGonnaDie <alexandersm@comcast.net>
2014-04-30 22:49:59 -0700

    moved config to new file, removed 2 globals

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ import random
 import time
 import sys
 import os
-import ConfigParser
 from var import *
+from configure import *
 
 #Functions
 def file_check(name):
@@ -26,63 +26,6 @@ def boolify(api):
     elif api == 'n':
         return False
     
-def configure():
-    if os.path.exists('config.ini'):
-        config.read('config.ini')
-        nick = config.get('Login', 'username')
-        password = config.get('Login', 'oauth')
-        message_after_number = config.getint('Twitch Messaging', 'Message After Amount')
-        message_number_timer = config.getfloat('Twitch Messaging', 'Message Amount Timer')
-        automated_message = config.get('Twitch Messaging', 'Automated Message')
-        twitch_api = config.getboolean("API's", 'twitch')
-        league_api = config.getboolean("API's", 'league')
-        osu_api = config.getboolean("API's", 'osu')
-    else:
-        nick = raw_input('Twitch Username: ')
-        password = raw_input('Twitch Oauth: ')
-        if 'oauth:' not in password:
-            password = 'oauth:' + password
-        message_after_number = raw_input('How many messages passed until automated message: ')
-        message_number_timer = raw_input('Minimum time before # of lines will trigger (in seconds): ')
-        automated_message = raw_input('Automated message: ')
-        print "You will need API keys for each API you wish to use."
-        print "Note the basic Twitch info grab does not require an API key."
-        twitch_api = raw_input('Use twitch api? y/n: ')
-        twitch_api.lower()
-        while twitch_api != 'y' and twitch_api != 'n':
-            print "Please enter 'y' or 'n'."
-            twitch_api = raw_input('Use twitch api? y/n: ')
-            twitch_api.lower()
-        league_api = raw_input('Use league api? y/n :')
-        league_api.lower()
-        while league_api != 'y' and league_api != 'n':
-            print "Please enter 'y' or 'n'."
-            league_api = raw_input('Use league api? y/n: ')
-            league_api.lower()
-        osu_api = raw_input('Use osu api? y/n: ')
-        osu_api.lower()
-        while osu_api != 'y' and osu_api != 'n':
-            print "Please enter 'y' or 'n'."
-            osu_api = raw_input('Use osu api? y/n: ')
-            osu_api.lower()
-        twitch_api = boolify(twitch_api)
-        league_api = boolify(league_api)
-        osu_api = boolify(osu_api)
-        config.add_section('Login')
-        config.set('Login', 'Username', nick)
-        config.set('Login', 'Oauth', password)
-        config.add_section('Twitch Messaging')
-        config.set('Twitch Messaging', 'Message After Amount', message_after_number)
-        config.set('Twitch Messaging', 'Message Amount Timer', message_number_timer)
-        config.set('Twitch Messaging', 'Automated Message', automated_message)
-        config.add_section("API's")
-        config.set("API's", 'Twitch', twitch_api)
-        config.set("API's", 'League', league_api)
-        config.set("API's", 'Osu', osu_api)
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
-    return nick, password, message_after_number, message_number_timer, automated_message
-
 def irc_connect(host,port,nick,password,channel):
 
     print "Connecting to channel: {channel}\nOn IRC server: {server}\nOn port: {port}\nWith Name: {name}"\
@@ -168,7 +111,6 @@ def send_after_number_time():
 def quote_retrieve():
     file_name = 'quotes'
     file_check(file_name)
-    global quote
     quote_lines = sum(1 for line in open('quotes.txt', 'r'))
     if quote_lines == 0:
         quote = 'No quotes added.'
@@ -178,11 +120,11 @@ def quote_retrieve():
         select_quote = random.randrange(1, quote_lines, 1)
         quote = open('quotes.txt', 'r').readlines()
         quote = quote[select_quote]
+    return quote
 
 def pun_retrieve():
     file_name = 'puns'
     file_check(file_name)
-    global pun
     pun_lines = sum(1 for line in open('puns.txt', 'r'))
     if pun_lines == 0:
         pun = 'No puns added.'
@@ -192,11 +134,11 @@ def pun_retrieve():
         select_pun = random.randrange(1, pun_lines, 1)
         pun = open('puns.txt', 'r').readlines()
         pun = pun[select_pun]
+    return pun
 
 
 
 #Main Program
-config = ConfigParser.RawConfigParser()
 nick, password, message_after_number, message_number_timer, automated_message = configure()
 
 channel = raw_input('Enter Channel to Join: ')
@@ -298,7 +240,7 @@ while loop == 1 and not destroy_loop:
 
     if messages.find('!quote') != -1:
         while previous_quote == quote:
-            quote_retrieve()
+            quote = quote_retrieve()
         send_message(quote)
         previous_quote = quote
 
@@ -313,7 +255,7 @@ while loop == 1 and not destroy_loop:
 
     if messages.find('!pun') != -1:
         while previous_pun == pun:
-            pun_retrieve()
+            pun = pun_retrieve()
         previous_quote = pun
         send_message(pun)
 
