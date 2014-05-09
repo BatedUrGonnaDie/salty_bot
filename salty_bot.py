52414b401d87913956ffed47fec74c4ecc409009
BatedUrGonnaDie <alexandersm@comcast.net>
2014-05-09 16:16:49 -0700

    :p

--- salty_bot.py
+++ salty_bot.py
@@ -11,8 +11,9 @@ class SaltyBot:
     messages_received = 0
     message_limit = 0
 
-    def __init__(self):
+    def __init__(self, config_data):
         self.irc = socket.socket()
+        self.config_data = config_data
 
     def twitch_connect(self):
         self.irc.connect((self.host, self.port))
@@ -34,33 +35,21 @@ class SaltyBot:
 def main():
     #Config reader
     config = ConfigParser.SafeConfigParser()
-    print config.read('channels.ini')
+    config.read('channels.ini')
     
     #Handling arrays
-    channels = []
+    channels = {}
     irc_channels = []
     
     for section_name in config.sections():
-        channels.append(section_name)
-        print section_name
+        channels[section_name] = {}
         for name, value in config.items(section_name):
-            print '{} = {}'.format(name,value)
+            channels[section_name][name] = value
+            channels[section_name]['channel'] = section_name
         
-    for channel in channels:
-        irc_channels.append('#'+channel)
-    '''
-    config.read('config.ini')
-    base_configure = {}
+    for i in channels.values():
+        SaltyBot(channels[i])
     
-    base_configure['twitch_nick'] = config.get('Login', 'twitch_nick')
-    base_configure['twitch_oauth'] = config.get('Login', 'oauth')
-    
-    base_configure['osu_nick'] = config.get('Login', 'osu_nick')
-    base_configure['osu_pass'] = config.get('Login', 'osu_pass')
-
-    for channel in irc_channels:
-        SaltyBot.twitch_run()#OKAY HERE WAS YOUR PROBLEM, YOU DIDN'T CALL THE COMMAND, YOU NEEDED THE '()' ALSO 
-    '''
     
 if __name__ == '__main__':
     main()
