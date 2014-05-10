b8691038e312156348ed30271727173c14065c56
BatedUrGonnaDie <alexandersm@comcast.net>
2014-05-10 02:59:12 -0700

    dead bot is dead

--- salty_bot.py
+++ salty_bot.py
@@ -13,44 +13,63 @@ class SaltyBot:
 
     def __init__(self, config_data):
         self.irc = socket.socket()
-        self.host = 'irc.twitch.tv'
+        self.config_data = config_data
+        self.twitch_host = 'irc.twitch.tv'
         self.port = 6667
+        print config_data
         self.twitch_nick = config_data['twitch_nick']
         self.twitch_oauth = config_data['twitch_oauth']
-        print self.host
+
+    def twitch_check(self):
+        self.url = 'https://api.twitch.tv/kraken/streams/'+self.twitch_nick
+        self.headers = {'Accept' : 'application/vnd.twitchtv.v2+json'}
+        self.data = requests.get(self.url, headers = self.headers)
+        self.data.decode = self.data.json()
+        self.data_stream = self.data_decode['stream']
+        if self.data_stream == None:
+            self.game = ''
+            self.title = ''
+            return self.game, self.title
+        else:
+            self.data_channel = self.data_stream['channel']
+            self.game = self.data_stream['game']
+            self.title = self.data_channel['status']
+            return self.game, self.title
 
     def twitch_connect(self):
-        print self.host
-        print self.port
-        self.irc.connect((self.host, self.port))
-        self.irc.send('PASS {}\r\n'.format(self.twitch_password))
+        self.irc.connect((self.twitch_host, self.port))
+        self.irc.send('PASS {}\r\n'.format(self.twitch_oauth))
         self.irc.send('NICK {}\r\n'.format(self.twitch_nick))
+        self.irc.send('JOIN #{}\r\n'.format(self.config_data['channel']))
 
     def twitch_run(self):
+        self.game, self.title = self.twitch_check()
+        print self.game
+        print self.title
         self.twitch_connect()
 
         while True:
             message = self.irc.recv(4096)
             print message
-
+    
 def main():
     #Config reader
     config = ConfigParser.SafeConfigParser()
     config.read('channels.ini')
     
     #Handling arrays
-    channels = {}
+    channel_configs = {}
     bots = []
     
     for section_name in config.sections():
-        channels[section_name] = {}
+        channel_configs[section_name] = {}
         for name, value in config.items(section_name):
-            channels[section_name][name] = value
-            channels[section_name]['channel'] = section_name
-        
-    for channels in channels.values():
-        bots.append(SaltyBot(channels))
-        
+            channel_configs[section_name][name] = value
+            channel_configs[section_name]['channel'] = section_name
+
+    for channels in channel_configs.values():
+        bots.append(SaltyBot(channel_configs))
+
     for bot in bots:
         tmp = threading.Thread(target=bot.twitch_run)
         tmp.daemon = True
@@ -58,6 +77,7 @@ def main():
 
     while True:
         time.sleep(1)
+        
 
 if __name__ == '__main__':
     main()
