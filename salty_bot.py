f51d6ce29f231788f097662492174cde81a31682
BatedUrGonnaDie <alexandersm@comcast.net>
2014-05-22 03:22:51 -0700

    added osu lookup, added some of old functionality

--- salty_bot.py
+++ salty_bot.py
@@ -12,20 +12,18 @@ class SaltyBot:
 
     def __init__(self, config_data):
         self.config_data = config_data
-        self.channel_config = self.config_reader(self.config_data)
         self.irc = socket.socket()
         self.config_data = config_data
         self.twitch_host = 'irc.twitch.tv'
         self.port = 6667
         self.twitch_nick = config_data['twitch_nick']
         self.twitch_oauth = config_data['twitch_oauth']
+        self.osu_api_key = config_data['osu_api_key']
         self.channel = config_data['channel']
         self.fo = open('{}_admins.txt'.format(self.channel), 'a+')
         self.fo.close()
         self.fo = open('{}_quotes.txt'.format(self.channel), 'a+')
         self.fo.close()
-    def config_reader(self, initial_config):
-        pass
 
     def twitch_check(self):
         self.url = 'https://api.twitch.tv/kraken/streams/'+self.channel
@@ -45,16 +43,34 @@ class SaltyBot:
             self.title = self.data_channel['status']
             return self.game, self.title
 
+    def osu_api_user(self):
+        self.url = 'https://osu.ppy.sh/api/get_user?k={}&u={}'.format(self.osu_api_key, self.channel)
+        self.data = requests.get(self.url)
+        
+        self.data_decode = self.data.json()
+        self.data_decode = self.data_decode[0]
+        
+        self.username = self.data_decode['username']
+        self.level = self.data_decode['level']
+        self.level = round(float(self.level))
+        self.level = int(self.level)
+        self.accuracy = self.data_decode['accuracy']
+        self.accuracy = round(float(self.accuracy), 2)
+        self.pp_rank = self.data_decode['pp_rank']
+        
+        self.response = '{} is level {} with {}% accuracy and ranked {}.'.format(self.username, self.level, self.accuracy, self.pp_rank)
+        self.twitch_send_message(self.response)
+
     def twitch_connect(self):
         self.irc.connect((self.twitch_host, self.port))
         self.irc.send('PASS {}\r\n'.format(self.twitch_oauth))
         self.irc.send('NICK {}\r\n'.format(self.twitch_nick))
         self.irc.send('JOIN #{}\r\n'.format(self.config_data['channel']))
 
-    def send_message(self, response):
+    def twitch_send_message(self, response):
         self.to_send = 'PRIVMSG #{} :{}\r\n'.format(self.channel, self.response)
         self.to_send = self.to_send.encode('utf-8')
-        self.irc.send(to_send)
+        self.irc.send(self.to_send)
         time.sleep(1.5)
 
     def twitch_run(self):
@@ -79,8 +95,12 @@ class SaltyBot:
                     self.sender = self.message.split(':')[1].split('!')[0]
                     self.message_body = ':'.join(self.message.split(':')[2:])
                     self.messages_received += 1
-
                     
+                    if self.message_body.startswith('!'):
+                        self.message_body = self.message_body.split('!')[-1]
+                        if self.message_body == 'rank':
+                            self.osu_api_user()
+                            
                     
                 elif self.action == 'MODE':
                     if '+o ' in self.message:
@@ -95,19 +115,26 @@ class SaltyBot:
                             self.fo.close()
 
 
-def osu_main():
+
+def osu_main(osu_nick, osu_irc_pass):
+    irc = socket.socket()
     osu_host = 'irc.ppy.sh'
     osu_port = 6667
-    irc = socket.socket()
     irc.connect((osu_host, osu_port))
     irc.send('PASS {}\r\n'.format(osu_irc_pass))
-    irc.send('NICK batedurgonnadie\r\n')
+    irc.send('NICK {}\r\n'.format(osu_nick))
     while True:
+        
+        def osu_send_message(channel, response):
+            irc.send('PRIVMSG {} :{}\r\n'.format(channel, response))
+        
         messages = irc.recv(4096)
-        messages = messages.split('\r\n')[0]
+        messages.split('\r\n')[0]
+        
         if messages.startswith('PING'):
             irc.send('PONG irc.ppy.sh')
-    
+        
+                  
 def main():
     #Config reader
     config = ConfigParser.SafeConfigParser()
@@ -122,6 +149,8 @@ def main():
         for name, value in config.items(section_name):
             channel_configs[section_name][name] = value
             channel_configs[section_name]['channel'] = section_name
+            config.read('config.ini')
+            channel_configs[section_name]['osu_api_key'] = config.get('General', 'osu_api_key')
 
     for channels in channel_configs.values():
         bots.append(SaltyBot(channels))
@@ -130,9 +159,13 @@ def main():
         tmp = threading.Thread(target=bot.twitch_run)
         tmp.daemon = True
         tmp.start()
-    t1 = threading.Thread(target = osu_main)
+
+    osu_irc_pass = config.get('General', 'osu_irc_pass')
+    osu_nick = config.get('General', 'osu_nick')
+    t1 = threading.Thread(target = osu_main, args = (osu_nick, osu_irc_pass))
     t1.daemon = True
     t1.start()
+    
     while True:
         time.sleep(1)
         
