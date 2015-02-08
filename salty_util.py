4689a84514f95c14ca63363b3b5094f78473af58
Bombmask <yo.mike@hotmail.com>
2015-02-08 16:45:34 -0700

    Added new tables, fixed inheritence problems New table classes for salty_web intgratio. attached

--- salty_util.py
+++ salty_util.py
@@ -6,23 +6,34 @@ import psycopg2 as pg
 import psycopg2.extras as pge
 import urlparse
 from datetime import datetime
-
+import time
 class Database(object):
     """docstring for Database"""
     DB = None
     class Table:
         def __init__(self):
             self.lookup_tables = {}
+            self.cached_objects = {}
 
         def append(self, x):
             self.lookup_tables[x.__name__] = x
 
         def __getattr__(self, name):
+            requested_table = None
+            #Main goal: Return CACHED object
             try:
-                return self.lookup_tables[name](Database._make_cursor())
+                #Return if exists
+                requested_table = self.cached_objects[name]
 
             except KeyError:
-                raise AttributeError("name '"+name+"' is not in table")
+                if name not in self.lookup_tables:
+                    raise AttributeError("Class '"+name+"' does not exist or has not been registered")
+
+                requested_table = self.cached_objects[name] = self.lookup_tables[name](Database._make_cursor())
+
+            finally:
+                return requested_table
+
 
     tables = Table()
 
@@ -31,16 +42,21 @@ class Database(object):
         
     @classmethod
     def is_connected(Database):
-        return not Database.DB.closed
+        try:
+            return not Database.DB.closed
+        except AttributeError:
+            print "Database has not been initialized : Not connected"
+            return False
 
     @classmethod
     def connect_to(Database, URL):
-        if not Database.IsConnected():
+
+        if not Database.is_connected():
             try:
                 DB_URL = urlparse.urlparse(URL)
-
+                
                 Database.DB = pg.connect(
-                    database =  DB_URL.path,
+                    database =  DB_URL.path[1:],
                     user     =  DB_URL.username,
                     password =  DB_URL.password,
                     host     =  DB_URL.hostname,
@@ -78,32 +94,131 @@ class Database(object):
 
 
 class Query(object):
-    """docstring for query"""
+    """docstring for Query"""
     def __init__(self, cursor):
-        super(query, self).__init__()
+        super(Query, self).__init__()
         self.cursor = cursor
         self.query = None
 
-    def __call__(self, query = None):
-        if query is not None:
-            self.query = query
-
+    def __call__(self): 
         if self.query is None:
             raise ValueError("No specified query")
 
-    def __iter__(self):
-        pass
+        self._execute()
 
-    def __execute(self):
+        return self
+
+    def __iter__(self):
+        return self.cursor
+
+    def __str__(self):
+        tmp = self.cursor.fetchall()
+        if len(tmp) == 1:
+            return str(tmp[0])
+        elif len(tmp) > 1:
+            return tmp
+
+        else:
+            return None
+
+    def _execute(self):
         self.cursor.execute(self.query)
+
+    def _close(self):
+        self.cursor.close()
 
     
 @Database.attach    
 class Users(Query):
-    base_query = """SELECT * FROM users """
+    """docstring for Users table"""
+    base_query = """SELECT {interest} FROM users """
+    def __call__(self):
+        self.query = self.base_query.format(interest='*')
+        return super(Users, self).__call__()
 
+    
     def user_data(self, user):
-        self.query = self.base_query+"WHERE twitch_name ="+user
+        self.query = self.base_query.format(interest='*')+"WHERE twitch_name='"+user+"'"
+        self._execute()
+        return self
         
-    def user_id(self, user):
-Database.tables.Query
+    def user_id(self, username):
+        self.query = self.base_query.format(interest="id")+"WHERE twitch_name='"+user+"'"
+
+    def user_username(self, id):
+        self.query = self.base_query.format(interest="twitch_name")+"WHERE id='"+str(id)+"'"
+        self._execute()
+        return self
+
+@Database.attach
+class Settings(Query):
+    """docstring for Settings table"""
+    def __call__(self):
+        self.query = "SELECT * FROM settings"
+        return super(Settings, self).__call__()
+
+@Database.attach
+class Commands(Query):
+    """docstring for Commands table"""
+    def __call__(self):
+        self.query = "SELECT * FROM commands"
+        return super(Commands, self).__call__()
+
+@Database.attach
+class CustomCommands(Query):
+    """docstring for CustomCommands table"""
+    def __call__(self):
+        self.query = "SELECT * FROM custom_commands"
+        return super(CustomCommands, self).__call__()
+        
+@Database.attach
+class Textutils(Query):
+    """docstring for Textutils table"""
+    def __call__(self):
+        self.query = "SELECT * FROM textutils"
+        return super(Textutils, self).__call__()
+
+if __name__=="__main__":
+    def print_me(table_iterator):
+        assert(issubclass(table_iterator.__class__, Query))
+
+        for row in table_iterator():
+            for k,v in row.items():
+                if k == "user_id":
+                    try:
+                        print k,v,
+                        print Database.tables.Users.user_username(v)
+                    except:
+                        print None
+
+                else:
+                    print k,v
+            print
+
+    def write_me(table_iterator):
+        assert(issubclass(table_iterator.__class__, Query))
+        with open(table_iterator.__class__.__name__+".out.txt",'w') as fout:
+
+            for row in table_iterator():
+                for k,v in row.items():
+                    out = str(k)+' : '+str(v)
+
+                    if k == "user_id":
+                        try:
+                            fout.write(out)
+                            fout.write(Database.tables.Users.user_username(v))
+                        except:
+                            pass
+
+                    else:
+                        fout.write(out)
+                
+    try:
+
+        Database.connect_to("REDACTED POSTGRES URL")
+        write_me(Database.tables.Textutils)
+                   
+    finally:
+        Database.disconnect()
+
+    
