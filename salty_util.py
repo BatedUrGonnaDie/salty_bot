6f61c2fb5df0ab2c0018170ffdcdf2d7d237c9ba
Bombmask <yo.mike@hotmail.com>
2015-02-08 03:35:48 -0700

    Commiting to show changes Added attach patching system. Created new system for database integration COMMIT DOES NOT WORK WITH SALTY_BOT.PY YET. DO NOT USE Please don't hassle me for not making a finished commit ;_ ; there are lots of changes Created Query class for table spesific inheritence. Only thing left to do it implement full table classes and inject them to the main salty class.

--- salty_util.py
+++ salty_util.py
@@ -7,295 +7,103 @@ import psycopg2.extras as pge
 import urlparse
 from datetime import datetime
 
-class db_connection(object):
-    #Class to handle interactions with the DB itself
-    #Variable members
-    connected = False
-    url = None
-    connection = None
-    cursor = None
-    lastReturn = None
+class Database(object):
+    """docstring for Database"""
+    DB = None
+    class Table:
+        def __init__(self):
+            self.lookup_tables = {}
 
-    #python methods
-    def __init__(self):
-        urlparse.uses_netloc.append("postgres")
-        #TODO Add keys file URL get
-        self.url = urlparse.urlparse("")
-        print self.url
-        self.connect()
+        def append(self, x):
+            self.lookup_tables[x.__name__] = x
 
-    def __del__(self):
-        self.disconnect()
-
-    def __enter__(self):
-        if not self.connected:
-            self.connect()
-
-        return self
-
-    def __exit__(self, type_error, value, traceback):
-        if value == None:
-            self.commit_changes()
-            return True
-        else:
-            self.rollback_changes()
-            print type_error, value, traceback
-            return False
-
-    #Public methods
-    def connect(self):
-        if not self.connected:
+        def __getattr__(self, name):
             try:
-                self.connection = pg.connect(
-                    database=self.url.path[1:],
-                    user=self.url.username,
-                    password=self.url.password,
-                    host=self.url.hostname,
-                    port=self.url.port
+                return self.lookup_tables[name](Database._make_cursor())
+
+            except KeyError:
+                raise AttributeError("name '"+name+"' is not in table")
+
+    tables = Table()
+
+    def __init__(self, URL):
+        super(Database, self).__init__()
+        
+    @classmethod
+    def is_connected(Database):
+        return not Database.DB.closed
+
+    @classmethod
+    def connect_to(Database, URL):
+        if not Database.IsConnected():
+            try:
+                DB_URL = urlparse.urlparse(URL)
+
+                Database.DB = pg.connect(
+                    database =  DB_URL.path,
+                    user     =  DB_URL.username,
+                    password =  DB_URL.password,
+                    host     =  DB_URL.hostname,
+                    port     =  DB_URL.port
                 )
-                self.cursor = self.connection.cursor()
-                self.connected = True
 
             except pg.OperationalError:
                 print "[ERROR] Either there is no url or the database url failed to parse correctally"
-                self.connected = False
-                return True
+    @classmethod
+    def disconnect(Database):
+        Database.DB.close()
 
-    def disconnect(self):
-        print "Disconnecting "
-        self.cursor.close()
-        self.connection.close()
-        self.connected = False
+    @classmethod
+    def commit_changes(Database):
+        if Database.IsConnected():
+            Database.DB.commit()
 
-    def commit_changes(self):
-        self.connection.commit()
+    @classmethod
+    def rollback_changes(Database):
+        if Database.IsConnected():
+            Database.DB.cancel()
+            Database.DB.rollback()
 
-    def rollback_changes(self):
-        self.connection.rollback()
-
-    def get_cursor(self, with_statment_init = None):
-        if not self.connected:
-            self.connect()
-
-        return salty_cursor(self, with_statment_init)
-
-class salty_cursor:
-    #Extended cursor class for saltybot
-    #Constant members
-    salty_db_link = None
-    cursor_link = None
-
-    #Variable members
-    last_query = None
-
-    #With statment members
-    execute_param_pack = ()
-    #Python methods
-    def __init__(self, db_link, pack = None):
-        self.salty_db_link = db_link
-        self.cursor_link = self.salty_db_link.connection.cursor(cursor_factory=pge.RealDictCursor)
-
-        #prepare for with statment initialization. 
-        if pack:
-            self.execute_param_pack = pack
-             
-
-    def __del__(self):
-        self.cursor_link.close()
-
-    def __enter__(self):
-        if isinstance(self.execute_param_pack, str):
-            query_string = self.execute_param_pack
-            params = None
-
-        else:
-            query_string, params = self.execute_param_pack
+    @classmethod
+    def _make_cursor(Database):
+        return Database.DB.cursor(cursor_factory=pge.RealDictCursor)
 
 
-        print self.execute_param_pack
-        print self.execute(query_string, params)
-        return self
+    @classmethod
+    def attach(Database, table):
+        Database.tables.append(table)
+        return table
 
-    def __exit__(self, type_error, value, traceback):
-        if not type_error:
-            self.cursor_link.close()
-            return True
 
-        else:
-            print type_error, value, traceback
-            return False
 
-    #Public methods
-    def log(self):
-        tmp = self.last_query
 
-        self.last_query = self.cursor_link.query
+class Query(object):
+    """docstring for query"""
+    def __init__(self, cursor):
+        super(query, self).__init__()
+        self.cursor = cursor
+        self.query = None
 
-        return tmp
+    def __call__(self, query = None):
+        if query is not None:
+            self.query = query
 
-    def execute(self, query_string, params = None):
-        if params:
-            self.cursor_link.execute(query_string, params)
-            self.log()
-            return self.cursor_link.statusmessage
+        if self.query is None:
+            raise ValueError("No specified query")
 
-        self.cursor_link.execute(query_string)
-        self.log()
-        return self.cursor_link.statusmessage
+    def __iter__(self):
+        pass
 
-    def execute_many(self, query_string, param_list):
-        self.cursor_link.executemany(query_string, param_list)
-        self.log()
-        return 
+    def __execute(self):
+        self.cursor.execute(self.query)
 
-    def get(self, Amount = 1):
-        if Amount == 1:
-            return self.cursor_link.fetchone()
+    
+@Database.attach    
+class Users(Query):
+    base_query = """SELECT * FROM users """
 
-        elif Amount > 1:
-            return self.cursor_link.fetchmany(Amount)
-
-        else:
-            return None
-
-    def get_all(self, is_iterator = False):
-        #Plese please only use thie is iterator inside a for loop call
-        if is_iterator:
-            return self.cursor_link
-
-        else:
-            return self.cursor_link.fetchall()
-
-class textutils(object):
-    #Class is intended to be used to send things into the textutils table
-    #Constant members
-    TABLE_NAME = "textutils"
-    DB_LINK = None
-    #Variable members
-    cursor = False
-
-    #Python methods
-    def __init__(self, db_connection_obj):
-        #start up method
-        self.DB_LINK = db_connection_obj
-        ## Cursor object for sending querys to the DB table
-        self.cursor = self.DB_LINK.get_cursor
-        self.q_cursor = self.DB_LINK.get_cursor()
-
-    def __del__(self):
-        #clean up method
-        #Cursor cleans up itself in it's __del__ method, Need input on if I should explicity delete the object
-        del self.cursor #Anyway this cleans up the cursor
+    def user_data(self, user):
+        self.query = self.base_query+"WHERE twitch_name ="+user
         
-    #Public methods
-    def send_dict(self, txt_obj, server_actual_insert=True):
-        if "twitch_name" in txt_obj and "user_id" not in txt_obj:
-            print "Looking up twitch name..."
-            txt_obj["user_id"] = self.get_user_id(txt_obj["twitch_name"])
-
-            if txt_obj["user_id"] > 0:
-                print "found user \"{}\" with \"user_id\": {}".format(txt_obj["twitch_name"], txt_obj["user_id"])
-
-            else:
-                print "[Error] user \"{}\" not found".format(txt_obj["twitch_name"])
-
-        #print txt_obj Debug call
-        #cool python unpacking syntax
-        return self.send(**txt_obj, server_actual_insert)
-
-    def send(self, user_id, type, reviewed, text, created_at, updated_at, server_actual_insert=True):
-        if type not in ["Quote", "Pun"]:
-            raise NameError("Text type must be either \"Quote\" or \"Pun\"")
-            
-
-        query_string = """INSERT INTO {_TABLE_NAME}(user_id, type, reviewed, text, created_at, updated_at)
-        VALUES (%s,%s,%s,%s,%s,%s)
-        """.format(_TABLE_NAME=self.TABLE_NAME)
-
-        if server_actual_insert:
-            return self.q_cursor.execute(query_string, (user_id, type, reviewed, text, created_at, updated_at))
-        else:   
-            return self.q_cursor.cursor_link.mogrify(query_string, (user_id, type, reviewed, text, created_at, updated_at))
-        
-    def get_user_id(self, twitch_name):
-        #TODO
-        print self.cursor.execute("""SELECT id FROM users WHERE twitch_name==%s""", (twitch_name))
-        return self.cursor.get_all()
-
-    #Contant utilites section
-    ##I made this function to add quotes to the DB if it gets cleared by accadent or on purpose (from files)
-    def INSERT_ALL_TEXTS(self, user_name=''):
-        ##Temporary
-        files_directory_location = "../textutils/"
-        totalUserDict = []
-        if user_name is not '':
-            user_name = "WHERE twitch_name='{}' ".format(user_name)
-
-        with self.cursor("""SELECT twitch_name, id FROM users {} ORDER BY id ASC""".format(user_name)) as users_request:
-
-            for user in users_request.get_all(True):
-                print user["twitch_name"],"ID is :",user["id"]
-
-                for text_type in ["quote", "pun"]:
-                    for switch in ["", "_review"]:
-                        try:
-                            with open(files_directory_location+"{user}_{type}{review_switch}.txt".format(user=user["twitch_name"], type=text_type, review_switch=switch)) as fin:
-                                #print fin Debug print
-                                for text_line in fin:
-                                    user_dict = {
-                                        'twitch_name': user["twitch_name"],
-                                        'user_id'    : user["id"],
-                                        'type'       : text_type.title(),
-                                        'reviewed'   : (True if switch == "" else False),
-                                        'text'       : unicode(text_line.strip(), errors="ignore"),
-                                        'created_at' : datetime.utcnow(),
-                                        'updated_at' : datetime.utcnow()
-                                    }
-                                    user_dict["query_string"] = self.send_dict(user_dict,False)
-                                    totalUserDict.append(user_dict)
-                                    #print user_dict
-                                    print self.send_dict(user_dict)
-
-
-
-                        except IOError, e:
-                            print e.filename, "failed to open...\nContinuing...\n"
-
-        with open("../JSON_DOCS/"+"textutils.json", 'w') as fout:
-            fout.write(json.dumps(totalUserDict, default=str))
-        self.DB_LINK.commit_changes()
-                    
-    def READ_ALL_TEXTS(self, write_to_file=False,  twitch_name = ''):
-        files_directory_location = "../JSON_DOCS/"
-
-        if twitch_name is not '':
-            with self.cursor("SELECT id FROM users WHERE twitch_name={}".format(twitch_name)) as query:
-                twitch_name = "WHERE user_id={}".format(query.get()["id"])
-        
-
-        with self.cursor("SELECT * FROM textutils {}".format(twitch_name)) as tmp:
-            if write_to_file==file:
-                with open(files_directory_location+"textutils.json", 'w') as fout:
-                    fout.write(json.dumps(tmp.get_all(), default=str))
-
-            elif write_to_file==iter:
-                return tmp.get_all(True)
-
-            elif write_to_file==list:
-                returnList = []
-                for i in tmp.get_all(True):
-                    returnList.append(i)
-
-                return returnList
-
-            else:
-                for i in tmp.get_all(True):
-                    print i
-                    print
-
-class settings:
-    pass
-
-main_connection = db_connection()
-textTable = textutils(main_connection)
-
+    def user_id(self, user):
+Database.tables.Query
