121cf09d0c58e632c1cc444e61f536b1dcc8f691
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-06-07 02:30:01 -0700

    Update !help descriptions

--- salty_bot.py
+++ salty_bot.py
@@ -1232,6 +1232,8 @@ class SaltyBot(object):
     def help_text(self, c_msg):
         text = {
             "help":         ["!help <command>", "Displays help text for commands (<stuff> is required, <stuff?> is optional, exception is wr, splits, pb if you want later options all before must be present)."],
+            "blacklist":    ["!blacklist <name>", "[Broadcaster Only] Prevent a user from interacting with the bot in any way."],
+            "whitelist":    ["!whitelist <name>", "[Broadcaster Only] Remove a user from the blacklist."],
             "commands":     ["!commands", "Displays all active commands in the current channel."],
             "quote":        ["!quote", "Displays a random reviewed quote from your channel."],
             "addquote":     ["!addquote <quote>", "Add the selected text for review (broadcasters adding bypass review."],
@@ -1239,8 +1241,10 @@ class SaltyBot(object):
             "addpun":       ["!addpun <pun>", "Add the selected text for review (broadcasters adding bypass review."],
             "8ball":        ["!8ball <question>", "Ask the 8ball and get a response."],
             "uptime":       ["!uptime", "See how long the stream has been live for (must be live to get a time."],
+            "highlight":    ["!highlight <message?>", "Save a timestamp with optional message decscribing what you wanted to highlight."],
+            "show_highlight": ["!show_highlight", "[Broadcaster Only] Lists all highlights saved to this point and clears the queue."],
             "wr":           ["!wr <game shortcode?> <category?>", "Retrieve the world record for a game's category.  Will try to use game and title if stream is live, otherwise parameters are required."],
-            "pb":           ["!pb <user?> <game shortcode?> <category?>", "Retrieve users pb for a game's category.  Whill try and use game and title if stream is live, otherwise parameters are required."],
+            "pb":           ["!pb <user?> <game shortcode?> <category?>", "Retrieve users pb for a game's category.  Will try to use game and title if stream is live, otherwise parameters are required."],
             "leaderboard":  ["!leaderboard", "Adds Twitch game to speedrun.com url, may or may not work depending on how games are mad on speedrun.com"],
             "splits":       ["!splits <user?> <game?> <category?>", "Retrive pb splits from splits.io.  Will try and user game and category from title and streamer if live, else all are required."],
             "race":         ["!race", "Retrieve race information from speedrdunslive.com"],
