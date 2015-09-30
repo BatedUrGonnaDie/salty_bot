f5386e48bb789d2266b317a8897d06b1ef051d3f
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-09-30 01:42:56 -0700

    test

--- salty_bot.py
+++ salty_bot.py
@@ -1373,7 +1373,7 @@ class SaltyBot(object):
                 #Toobou trigger check
                 if c_msg["message"].lower().find(self.t_trig.lower()) != -1:
                     if self.config_data["toobou_active"] and self.config_data["toobou_output"] != "":
-                        if int(time.time()) - self.command_times["toobou"]["last"] >= self.command_times["toobout"]["limit"]:
+                        if int(time.time()) - self.command_times["toobou"]["last"] >= self.command_times["toobou"]["limit"]:
                             self.twitch_send_message(self.config_data["toobou_output"].replace("$sender", c_msg["sender"]))
                             self.command_times['toobou']['last'] = int(time.time())
 
