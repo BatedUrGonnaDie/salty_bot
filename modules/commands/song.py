52faac6dddb22685ffccfab368f0fe94414ed8a5
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-10-24 22:41:57 -0700

    formatting change

--- modules/commands/song.py
+++ modules/commands/song.py
@@ -4,5 +4,5 @@ def call(c_msg, **kwargs):
     success, response = kwargs["newbs_api"].get_song(c_msg["channel"][1:])
     if not success:
         return False, \
-            "Error retrieving data from LoN ({}).".format(response.status_code)
+            "Error retrieving data from LoN ({0}).".format(response.status_code)
     return True, response["song"]
