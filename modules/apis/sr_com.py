5cedd316d00cce8711d1b5077fa00043d3a05637
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-10-27 10:24:46 -0700

    Add speedrun.com api wrapper

---
+++ modules/apis/sr_com.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python2.7
+
+import modules.apis.api_base as api
+
+class SRcomAPI(api.API):
+
+    def __init(self, session = None):
+        super(SRcomAPI, self).__init__("http://www.speedrun.com/api/v1", session)
+
+    def get_user_pbs(self, user, embeds = "", **kwargs):
+        # Embeds should be list of parameters
+        endpoint = "/users/{0}/personal-bests?embed={1}".format(user, ",".join(embeds))
+        success, response = self.get(endpoint, **kwargs)
+        return success, response
