c8ce95dc7a102f214b698c54ff5d73fd06f9b9b9
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-11-08 21:26:18 -0800

    Remove dumb typecast

--- modules/commands/race.py
+++ modules/commands/race.py
@@ -48,7 +48,7 @@ def call(salty_inst, c_msg, **kwargs):
             send_msg += ", Finished {0}{1} with a time of {2}".format(
                 user_place, get_suffix.suffix(user_place), time_formatted)
         else:
-            time_formatted = time_formatter(int(time.time()) - race_time)
+            time_formatted = time_formatter.format(time.time() - race_time)
             send_msg += ", RaceBot Time: {0}".format(time_formatted)
     if race_status == "Complete":
         send_msg += ". {}".format(race_link)
