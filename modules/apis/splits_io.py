2f9324f4d073082f47ecd8279d4bd85eaa1cf258
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-01-22 06:16:27 -0800

    add splits-io api wrapper

---
+++ modules/apis/splits_io.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python2.7
+
+import modules.apis.api_base as api
+
+class SplitsIOAPI(api.API):
+
+    def __init__(self, session = None):
+        super(SplitsIOAPI, self).__init__("https://splits.io/api/v3", session)
+
+    def get_user_splits(self, user, **kwargs):
+        endpoint = "/users/{0}/pbs".format(user)
+        success, response = self.get(endpoint, **kwargs)
+        return success, response
