beb0bcf9f5a6fdf84120635241021506da386510
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-01-22 06:18:51 -0800

    commanding the commands command

---
+++ modules/commands/commands.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python2.7
+
+def call(salty_inst, c_msg, **kwargs):
+    active_commands = dict(salty_inst.commands)
+    if not salty_inst.is_live:
+        if "!uptime" in active_commands:
+            del active_commands["!uptime"]
+        if "!highlight" in active_commands:
+            del active_commands["!highlight"]
+    reg_commands = []
+    admin_commands = []
+    for i in active_commands.iteritems():
+        if i[1]["mod_req"]:
+            admin_commands.append(i[0])
+        else:
+            reg_commands.append(i[0])
+
+    command_string = ", ".join(sorted(reg_commands))
+    if admin_commands:
+        command_string += " | Mod Only Commands: {0}".format(", ".join(sorted(admin_commands)))
+    return True, command_string
