0d86151d63f684513cf80aa015b6a8a04770995a
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-01-22 06:15:41 -0800

    add compiled regex expressions

---
+++ modules/extensions/regexes.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python2.7
+
+import re
+
+OSU_URL = re.compile("http[s]{0,1}://osu.ppy.sh/([sb])/(\d+)")
+YOUTUBE_URL = re.compile("(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})")
