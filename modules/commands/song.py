cfdaa2abc4bf483461f1109ead81a851be65cb51
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-01-22 06:17:44 -0800

    update for new stuff

--- modules/commands/song.py
+++ modules/commands/song.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python2.7
 
-def call(c_msg, **kwargs):
-    success, response = kwargs["newbs_api"].get_song(c_msg["channel"][1:])
+def call(salty_inst, c_msg, **kwargs):
+    success, response = salty_inst.osu_api.get_song(c_msg["channel"][1:], **kwargs)
     if not success:
         return False, \
             "Error retrieving data from LoN ({0}).".format(response.status_code)
