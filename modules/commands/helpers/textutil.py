e3f15ccd8c8d45e7e9a5d01ed6188c5954f776a8
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-01-22 06:17:22 -0800

    quote/pun base code

---
+++ modules/commands/helpers/textutil.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python2.7
+
+def add(salty_inst, c_msg, text_type, **kwargs):
+    try:
+        text = c_msg["message"].split(" ", 1)[1].strip()
+    except IndexError:
+        return False, "Please do not leave the quote blank."
+
+    data = {
+        "reviewed" : 1 if c_msg["sender"] == salty_inst.channel else 0,
+        "text" : text,
+        "user_id" : salty_inst.user_id
+    }
+    success, response = salty_inst.newbs_api.add_textutil(salty_inst.channel, text_type, data, **kwargs)
+    if not success:
+        return False, "Error adding {0} to database ({1})".format(text_type, response.status_code)
+    return True, "{1} successfully added to the database.".format(text_type.capitalize())
+
+def show(salty_inst, c_msg, text_type, **kwargs):
+    success, response = salty_inst.newbs_api.show_textutil(salty_inst.channel, text_type, **kwargs)
+    if not success:
+        return False, \
+            "Error retrieving {0} from the database ({1})".format(text_type, response.status_code)
+    return True, response["{0}s".format(text_type)]["text"]
