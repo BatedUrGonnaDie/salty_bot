df95498203bc9adb01237878eb89c9283aed8474
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-01-22 06:18:26 -0800

    add colon to ignored diff ratio lambda

--- modules/commands/helpers/get_diff_ratio.py
+++ modules/commands/helpers/get_diff_ratio.py
@@ -3,6 +3,6 @@
 import difflib
 
 def diff_ratio(supplied, check_against):
-    custom_lambda = lambda x: x == " " or x == "_"
+    custom_lambda = lambda x: x == " " or x == "_" or x == ":"
     diff = difflib.SequenceMatcher(custom_lambda, supplied, check_against)
     return diff.ratio()
