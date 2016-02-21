f71655444a2e8a1540e260133896b0a96d3938f8
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-02-20 21:45:55 -0800

    implement !checkvote

---
+++ modules/commands/checkvote.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python2.7
+
+def call(salty_inst, c_msg, **kwargs):
+    votes = salty_inst.votes
+    if not votes:
+        return False, "No open vote."
+
+    response = 'Current poll: "{0}". '.format(votes["name"])
+    if not votes["options"]:
+        response += "No one has voted yet."
+    else:
+        response += "; ".join(
+            ["{0}: {1}".format(votes["casing"][k], v) for k, v in votes["options"].items()]
+        )
+
+    return True, response
