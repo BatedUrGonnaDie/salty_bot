9fb31595a481b3870c23a871716b553f2724e600
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-02-20 21:45:04 -0800

    poll name and type regexes

--- modules/extensions/regexes.py
+++ modules/extensions/regexes.py
@@ -4,3 +4,5 @@ import re
 
 OSU_URL = re.compile("http[s]{0,1}://osu.ppy.sh/([sb])/(\d+)")
 YOUTUBE_URL = re.compile("(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})")
+POLL_NAME = re.compile('"(.+)"')
+POLL_OPTIONS = re.compile("\((.+?)\)")
