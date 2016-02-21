1340506010731ab0c7d2806e5ca0948d17277a2a
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-02-20 21:45:17 -0800

    !vote implemented

---
+++ modules/commands/vote.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python2.7
+
+def call(salty_inst, c_msg, **kwargs):
+    if not salty_inst.votes:
+        return False, ""
+    try:
+        sender_vote = c_msg["message"].split(" ", 1)[-1]
+        vote_lower = sender_vote.lower()
+    except Exception:
+        return False, ""
+
+    if salty_inst.votes["type"] == "strict":
+        if vote_lower not in salty_inst.votes["options"]:
+            return False, "You must vote for one of the following options: {0}".format(
+                ", ".join(salty_inst.votes["options"].keys())
+            )
+
+    if c_msg["sender"] in salty_inst.votes["voters"]:
+        previous_vote = salty_inst.votes["voters"][c_msg["sender"]]
+        if vote_lower == previous_vote:
+            return False, ""
+        salty_inst.votes["options"][previous_vote] -= 1
+        if salty_inst.votes["options"][previous_vote] == 0 and salty_inst.votes["type"] == "loose":
+            del salty_inst.votes["options"]["previous"]
+
+        try:
+            salty_inst.votes["options"][vote_lower] += 1
+        except KeyError:
+            salty_inst.votes["options"][vote_lower] = 1
+
+        salty_inst.votes["voters"][c_msg["sender"]] = vote_lower
+        return True, "{0} has changed their vote to {1}.".format(
+            c_msg["sender"],
+            vote_lower
+        )
+    else:
+        try:
+            salty_inst.votes["options"][vote_lower] += 1
+        except KeyError:
+            salty_inst.votes["options"][vote_lower] = 1
+
+        salty_inst.votes["voters"][c_msg["sender"]] = vote_lower
+        return True, "{0} now has {1} votes.".format(
+            vote_lower,
+            str(salty_inst.votes["options"][vote_lower])
+        )
