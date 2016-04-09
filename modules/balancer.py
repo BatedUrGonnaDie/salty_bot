6a5c75c7d3a7f8a06b775cc8bc32069b653d280e
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-04-09 04:06:58 -0700

    Update the locking mechanism
    create and removing connections should not be called from outside the
    file.  They will be automatically called no connection exists for the
    bot or if the last bot on a connection is killed.

--- modules/balancer.py
+++ modules/balancer.py
@@ -10,34 +10,46 @@ class Balancer(object):
         self.connections = {}
         self.lock = threading.Semaphore()
 
-    def create_connection(self, username, oauth):
+    def _create_connection(self, username, oauth):
+        # Should only be called while the lock is acquired
         t_irc = twitch_irc.TwitchIRC(username, oauth)
         t_irc.create()
         t_irc.connect()
         t_irc_thread = threading.Thread(target=t_irc.main_loop, args=(self.process_incomming,))
         t_irc_thread.daemon = True
-        with self.lock:
-            self.connections[username] = {"thread" : t_irc_thread, "irc_obj" : t_irc, "bots" : {}}
-            t_irc_thread.start()
+        self.connections[username] = {"thread" : t_irc_thread, "irc_obj" : t_irc, "bots" : {}}
+        t_irc_thread.start()
 
-    def remove_connection(self, username):
+    def _remove_connection(self, username):
+        # Should only be called while the lock is acquired
+        self.connections[username]["irc_obj"].continue_loop = False
+        self.connections[username]["thread"].join()
+        del self.connections[username]
+
+    def retrieve_bot_config(self, twitch_name):
         with self.lock:
-            self.connections[username]["irc_obj"].continue_loop = False
-            self.connections[username]["thread"].join()
-            del self.connections[username]
+            return self.connections["bots"][twitch_name].config
 
 
     def add_bot(self, bot):
         with self.lock:
+            if not self.connections.get(bot.bot_nick, None):
+                self._create_connection(bot.bot_nick, bot.bot_oauth)
+
             self.connections[bot.bot_nick]["bots"][bot.channel] = bot
             self.connections[bot.bot_nick]["irc_obj"].join(bot.channel)
+
+    def update_bot(self, new_config):
+        with self.lock:
+            self.connections["bots"][new_config["twitch_name"]].update_config(new_config)
 
     def remove_bot(self, bot_name, channel_name):
         with self.lock:
             self.connections[bot_name]["irc_obj"].part(channel_name)
             del self.connections[bot_name]["bots"][channel_name]
-            if not self.connections[bot_name]["bots"]:
-                self.remove_connection(bot_name)
+            bots_left = self.connections[bot_name].get("bots", None)
+            if not bots_left:
+                self._remove_connection(bot_name)
 
     def process_incomming(self, c_msg):
         with self.lock:
