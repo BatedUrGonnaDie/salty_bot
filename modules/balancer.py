420e49f1c6fd97217a99cc747ac13bd08fb4ba76
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-04-24 03:48:50 -0700

    Rework how updating bot configs work
    Also handle the twitch specific RECONNECT command inside of the balancer
    (this may move out of the balancer in the future to make it more
    generic)

--- modules/balancer.py
+++ modules/balancer.py
@@ -3,6 +3,8 @@
 import threading
 
 from modules import twitch_irc
+from modules.module_errors import DeactivatedBotException
+from modules.module_errors import NewBotException
 
 class Balancer(object):
 
@@ -11,7 +13,8 @@ class Balancer(object):
         self.lock = threading.Semaphore()
 
     def _create_connection(self, username, oauth):
-        # Should only be called while the lock is acquired
+        # Should only be called while the lock is acquired with fresh data
+        # i.e. With the same lock as when you check that there is no connection
         t_irc = twitch_irc.TwitchIRC(username, oauth)
         t_irc.create()
         t_irc.connect()
@@ -21,17 +24,15 @@ class Balancer(object):
         t_irc_thread.start()
 
     def _remove_connection(self, username):
-        # Should only be called while the lock is acquired
+        # Should only be called while the lock is acquired with fresh data
+        # i.e. With the same lock as when you check that there is no more bots on a connection
         self.connections[username]["irc_obj"].continue_loop = False
         self.connections[username]["thread"].join()
         del self.connections[username]
 
-    def retrieve_bot_config(self, twitch_name):
-        with self.lock:
-            return self.connections["bots"][twitch_name].config
-
-
     def add_bot(self, bot):
+        # This will overwrite any bot under the exact same name in the same channel
+        # Up to implementer to check for conflictions
         with self.lock:
             if not self.connections.get(bot.bot_nick, None):
                 self._create_connection(bot.bot_nick, bot.bot_oauth)
@@ -41,18 +42,29 @@ class Balancer(object):
 
     def update_bot(self, new_config):
         with self.lock:
-            self.connections["bots"][new_config["twitch_name"]].update_config(new_config)
+            try:
+                self.connections["bots"][new_config["twitch_name"]].update_config(new_config)
+            except KeyError:
+                raise NewBotException
+            except DeactivatedBotException:
+                # We need to remove the bot from the list
+                raise
 
     def remove_bot(self, bot_name, channel_name):
         with self.lock:
             self.connections[bot_name]["irc_obj"].part(channel_name)
             del self.connections[bot_name]["bots"][channel_name]
-            bots_left = self.connections[bot_name].get("bots", None)
-            if not bots_left:
+            if not self.connections[bot_name].get("bots", None):
                 self._remove_connection(bot_name)
 
     def process_incomming(self, c_msg):
         with self.lock:
+            if c_msg["action"] == "RECONNECT":
+                irc_obj = self.connections[c_msg["bot_name"]]["irc_obj"]
+                old_host = irc_obj.host
+                irc_obj.host = irc_obj.get_peer_ip()
+                irc_obj.reconnect()
+                irc_obj.host = old_host
             bot_obj = self.connections[c_msg["bot_name"]]["bots"][c_msg["channel"][1:]]
         outbound = bot_obj.process_message(c_msg)
         if outbound:
