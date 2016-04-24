720dba0fb9bfe4d64375bcb2aae16e422fef2ce3
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-04-24 03:44:56 -0700

    New test folder

---
+++ tests/test_salty_bot.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python2.7
+
+import pytest
+
+from modules import irc
+from modules import twitch_irc
+
+class TestSalty(object):
+
+    def test_irc_message_parsing(self):
+        assert 1
