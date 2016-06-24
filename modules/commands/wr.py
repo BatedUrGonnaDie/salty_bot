9137af126231a62c1c5c23f23b379e57ccd0d840
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-06-24 04:38:59 -0700

    actually works now

--- modules/commands/wr.py
+++ modules/commands/wr.py
@@ -21,17 +21,29 @@ def call(salty_inst, c_msg, **kwargs):
         category = salty_inst.title
 
     if search_game:
-        success, response = salty_inst.sr_com_api.get_games_search(game)
+        success, response = salty_inst.sr_com_api.get_games_search(game, embeds=["categories"])
         if not success:
             return False, \
                 "Error retrieving games from speedrun.com ({0}).".format(response.status_code)
+
         for i in response["data"]:
             if i["names"]["international"] == game:
-                found_categories = [{x["name"] : x["id"]} for x in i["categories"]["data"] if x["type"] == "per-game"]
+                found_categories = {x["name"] : x["id"] for x in i["categories"]["data"] if x["type"] == "per-game"}
                 game_id = i["id"]
                 break
         else:
             return False, "No game found for: {0}.".format(game)
+    else:
+        success, response = salty_inst.sr_com_api.get_games({"abbreviation":game}, embeds=["categories"])
+        if not success:
+            try:
+                decode = response.json()
+            except ValueError:
+                decode = {"message": "Game {0} could not be found".format(game), "status" : 404}
+            return False, "{0} ({1})".format(decode["message"], decode["status"])
+
+        found_categories = {x["name"] : x["id"] for x in response["data"][0]["categories"]["data"] if x["type"] == "per-game"}
+        game_id = response["data"][0]["id"]
 
     if infer_category:
         category_finder = get_category_title.find_active_cat
@@ -39,10 +51,17 @@ def call(salty_inst, c_msg, **kwargs):
         category_finder = get_category_string.find_active_cat
 
     cat_success, cat_response = category_finder(found_categories, category)
+
     if not cat_success:
         return False, cat_response
 
-    success, response = salty_inst.sr_com_api.get_leaderboards(game_id, found_categories[cat_response], **kwargs)
+    success, response = salty_inst.sr_com_api.get_leaderboards(
+        game_id,
+        found_categories[cat_response],
+        embeds=["game", "players"],
+        params={"top" : 1},
+        **kwargs
+    )
     if not success:
         return False, \
             "Error retrieving leaderboard from speedrun.com ({0}).".format(response.status_code)
@@ -50,13 +69,13 @@ def call(salty_inst, c_msg, **kwargs):
     msg = "The current world record for {0} {1} is {2} by {3}.".format(
         response["data"]["game"]["data"]["names"]["international"],
         cat_response,
-        time_formatter.format(response["runs"]["times"]["primary_t"]),
-        response["players"]["data"][0]["names"]["international"]
+        time_formatter.format_time(response["data"]["runs"][0]["run"]["times"]["primary_t"]),
+        response["data"]["players"]["data"][0]["names"]["international"]
     )
-    if response["runs"][0]["run"]["videos"]:
-        msg += " Video: {0}".format(response["runs"][0]["run"]["videos"]["links"][0]["uri"])
-    if response["runs"][0]["run"]["splits"]:
-        msg += " Splits: {0}".format(response["runs"][0]["run"]["splits"]["uri"])
+    if response["data"]["runs"][0]["run"]["videos"]:
+        msg += " Video: {0}".format(response["data"]["runs"][0]["run"]["videos"]["links"][0]["uri"])
+    if response["data"]["runs"][0]["run"]["splits"]:
+        msg += " Splits: {0}".format(response["data"]["runs"][0]["run"]["splits"]["uri"])
 
     return True, msg
 
