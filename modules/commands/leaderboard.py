39e0db07762a6dd5545381dde0d2b8237a799f0a
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-06-24 05:21:08 -0700

    fix bugs in !leaderboard

--- modules/commands/leaderboard.py
+++ modules/commands/leaderboard.py
@@ -5,12 +5,13 @@ from modules.commands.helpers import get_diff_ratio
 def call(salty_inst, c_msg, **kwargs):
     msg_split = c_msg["message"].split(" ", 1)
     try:
-        game = msg_split[2]
+        game = msg_split[1]
         twitch_game = False
     except IndexError:
         game = salty_inst.game
         twitch_game = True
-    success, response = salty_inst.sr_com_api.get_games(game, **kwargs)
+    success, response = salty_inst.sr_com_api.get_games({"name" : game}, **kwargs)
+
     if not success:
         return False, \
             "Error retrieving info from speedrun.com ({0})".format(response.status_code)
@@ -21,10 +22,11 @@ def call(salty_inst, c_msg, **kwargs):
                 game_record = i
                 break
         else:
-            if get_diff_ratio.diff_ratio(game.lower(), i["name"]["international"].lower()) > .8:
+            if get_diff_ratio.diff_ratio(game.lower(), i["names"]["international"].lower()) > .8:
                 game_record = i
                 break
     else:
+        print "No game found."
         return False, "Could not find a suitable game match for {0}".format(game)
 
     return True, game_record["weblink"]
