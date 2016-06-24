1413087599e07def6a1491d529b830c5f4de930d
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-06-24 05:18:22 -0700

    better describe what it does

--- modules/apis/splits_io.py
+++ modules/apis/splits_io.py
@@ -7,7 +7,7 @@ class SplitsIOAPI(api.API):
     def __init__(self, session = None):
         super(SplitsIOAPI, self).__init__("https://splits.io/api/v3", session)
 
-    def get_user_splits(self, user, **kwargs):
+    def get_user_pbs(self, user, **kwargs):
         endpoint = "/users/{0}/pbs".format(user)
         success, response = self.get(endpoint, **kwargs)
         return success, response
