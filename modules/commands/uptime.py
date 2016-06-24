e33837770d150e0f858d23141ca202001af767a0
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-06-24 04:40:10 -0700

    add !uptime

---
+++ modules/commands/uptime.py
@@ -0,0 +1,14 @@
+#! /usr/bin/evn python2.7
+
+import datetime
+
+import isodate
+import pytz
+
+def call(salty_inst, c_msg, **kwargs):
+    if not salty_inst.is_live:
+        return True, "Stream currently offline.  Follow to receive notifications for when I am."
+
+    start = isodate.parse_datetime(salty_inst.stream_start)
+    current = datetime.datetime.now(pytz.utc)
+    return True, "The stream has been live for {0}.".format(str(current - start)[:-7])
