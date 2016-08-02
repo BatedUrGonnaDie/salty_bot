af58d2019273e4ec1ed3b8c1473bcc1e1abd1e24
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-08-02 03:18:05 -0700

    fix another bug

--- modules/helpers/osu_chat_link.py
+++ modules/helpers/osu_chat_link.py
@@ -5,19 +5,21 @@ import re
 import modules.extensions.regexes as regexes
 import modules.commands.helpers.time_formatter as time_formatter
 
-def call(salty_inst, c_msg, **kwargs):
+ON_ACTION = "PRIVMSG"
+
+def call(salty_inst, c_msg, balancer, **kwargs):
     beatmaps = re.findall(regexes.OSU_URL, c_msg["message"])
     final_list = []
     for i in beatmaps:
         success, response = salty_inst.osu_api.get_beatmap("{0}={1}".format(i[0], i[1]), **kwargs)
         if not success:
             continue
-        final_list.append("[{0}] {1} - {2}, mapped by {3} ({4} stars".format(
+        final_list.append("[{0}] {1} - {2}, mapped by {3} ({4} stars)".format(
             time_formatter.format_time(response[0]["total_length"]),
             response[0]["artist"],
             response[0]["title"],
             response[0]["creator"],
-            round(response[0]["difficultyrating"], 2)
+            round(float(response[0]["difficultyrating"]), 2)
         ))
 
     if len(final_list) == 0:
