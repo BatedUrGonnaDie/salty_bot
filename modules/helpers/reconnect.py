98f7fab4562614425e09d6e59839286d40f45e2f
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-08-02 03:17:56 -0700

    move reconnect to its own helper

---
+++ modules/helpers/reconnect.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python2.7
+
+ON_ACTION = "RECONNECT"
+
+def call(salty_inst, c_msg, balancer, **kwargs):
+    with balancer.lock:
+        irc_obj = balancer.connections[c_msg["bot_name"]]["irc_obj"]
+        old_host = irc_obj.host
+        irc_obj.host = irc_obj.get_peer_ip()
+        irc_obj.reconnect()
+        irc_obj.host = old_host
+
+    return True, ""
