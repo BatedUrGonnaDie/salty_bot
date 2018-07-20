a49bfe8d245ef49a4d2fec314ecce8c4d145e3d8
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2018-07-20 11:41:43 -0700

    catch index error for messages with really short links

--- modules/twitch_irc.py
+++ modules/twitch_irc.py
@@ -36,8 +36,11 @@ class TwitchIRC(irc.IRC):
     def privmsg(self, channel, msg):
         self.clear_limit()
         if not self.rate_limited:
-            if msg[0] in (".", "/") and msg[1:4] != "me ":
-                msg = "\\{0}".format(msg)
+            try:
+                if (msg.startswith(".") or msg.startswith("/")) and msg[1:4] != "me ":
+                    msg = "\\{0}".format(msg)
+            except IndexError:
+                pass
             super(TwitchIRC, self).privmsg(channel, msg)
             self.sent_messages += 1
             self.total_messages += 1
